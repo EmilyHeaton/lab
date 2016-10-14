1f45d305f5b5fc2542f04a14cec7f05cd0677872
Emily Heaton <heaton@iastate.edu>
2016-10-14 10:08:20 -0500

    no message

--- week_08/data_cleaning/script.R
+++ week_08/data_cleaning/script.R
@@ -2,21 +2,27 @@
 
 #First, read it in to make it a dataframe with one header
 #Select the line with the most useful information 
-header_weather<-scan("weather.txt")
+header_weather<-scan("weather.txt",skip = 1, sep= "\t", nlines = 1, what= character())
 #Select all the data
-weather<-read.table("weather.txt")
+weather<-read.table("weather.txt", skip= 4, na.strings = "?", sep = "\t", stringsAsFactors = FALSE)
 #Put the header and the data together
 names(weather)<-header_weather
 
 #Lets just work with time, total rainfall, total solar radiation, max air temp, and min air temp -- select these columns
 
+weat<- weather[,c(1,6,10,11,13)]
+
 #Give the column new names -- "date", "total_rain_in", "total_rad_MJ", "max_temp_F", "min_temp_F"
+
+names(weat)<- c("date", "total_rain_in", "total_rad_MJ", "max_temp_F", "min_temp_F")
 
 #Filter the data to make sure you have reasonable values
   #Between 0 and 6 for total rain
-  #Between 60 and 100 for total radiation
-  #Between -50 and -10 for both temps
+  #Between 30 and 70 for total radiation
+  #Between 0 and 100 for max temps
   #max temp cannot be lower than min temp
+
+temp<-weat[weat$total_rain_in %in% c(0:6),]
 
 #Create a rain column in millimeters and temperature columns in degrees C
 
